9ace54855fe6bafb61c29391579e9c406ac201ab
RaccoonCode96 <waawoo13@gmail.com>
2021-09-03 22:19:54 +0900

    Add: Tabs 상단에서 두번째로 있는 nav (home 화면에서 article 이동을 담당)

---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,32 @@
+import './Tabs.scss';
+import { CloseOutlined } from '@ant-design/icons';
+
+export const Tab = ({ Icon, title, active, color, type, href }) => {
+	return (
+		<a
+			href={href}
+			className="tab_cntr"
+			style={active && { backgroundColor: '#1e1e1e' }}
+		>
+			<div className="tab">
+				<Icon className="icon" style={color && { color }} />
+				<span className="tab_title" style={active && { color: '#68C977' }}>
+					{title}
+				</span>
+				<span className="title_type" style={active && { color: '#68C977' }}>
+					{type}
+				</span>
+			</div>
+			<div className="end">
+				{active && <div className="dot"></div>}
+				<CloseOutlined className="close" />
+			</div>
+		</a>
+	);
+};
+
+const Tabs = ({ children }) => {
+	return <nav className="tabs">{children}</nav>;
+};
+
+export default Tabs;
