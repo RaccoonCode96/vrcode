8ff65b15df2bf7979abe5fd8121fc83fd1a7d5c4
RaccoonCode96 <waawoo13@gmail.com>
2021-09-03 22:17:08 +0900

    Add: start (처음 시작을 담당하는 main의 article)

---
+++ src/components/Mains/Start.jsx
@@ -0,0 +1,18 @@
+import './main.scss';
+import racoon from '../../images/raccoon.jpg';
+
+const Start = () => {
+	return (
+		<article className="sub_content start" id="start">
+			<div className="content">
+				<h1 className="sub_content_title">Start</h1>
+				<div className="welcome">Welcome to Visual Raccoon Code !</div>
+				<div className="name blue">TaeYoung (Raccoon)</div>
+				<div className="fe blue">💻 Front-end Developer</div>
+			</div>
+			<img src={racoon} alt="profile_image" className="profile_image" />
+		</article>
+	);
+};
+
+export default Start;
