af1e01f46e65d586a92cdd298e478478ee234d26
RaccoonCode96 <waawoo13@gmail.com>
2021-09-05 22:53:47 +0900

    Update: main의 Project ProjectLayout 사용 형태로 변경

--- src/components/Mains/Project.jsx
+++ src/components/Mains/Project.jsx
@@ -1,35 +1,53 @@
-import movie from '../../images/movie_app.gif';
 import racstagram_v1 from '../../images/racstagram_v1.gif';
+import racstagram_v2 from '../../images/racstagram_v2.gif';
 import paint from '../../images/paint_app.gif';
-import starbucks from '../../images/starbucks_site.gif';
+import todoList from '../../images/todo_list.png';
+import ProjectLayout from '../Common/ProjectLayout';
+import { ReactComponent as ReactIcon } from '../../images/react_icon.svg';
+import { ReactComponent as HtmlIcon } from '../../images/html_icon.svg';
+import { ReactComponent as JsIcon } from '../../images/js_icon.svg';
+import { ReactComponent as CssIcon } from '../../images/css_icon.svg';
+import { ReactComponent as ReduxIcon } from '../../images/redux_icon.svg';
+import { ReactComponent as Firebase } from '../../images/firebase_icon.svg';
 
-const Project = () => (
-	<article className="sub_content project" id="project">
-		<div className="content">
-			<h1 className="sub_content_title">Project</h1>
-			<div className="projects_cntr">
-				<div className="project">
-					Racstagram ver2 - redux Toolkit, 더 많은 기능
-				</div>
-				<div className="project">
-					<img src={racstagram_v1} alt="racstagram" className="project_img" />
-					Racstagram ver1 - only react
-				</div>
-				<div className="project">
-					<img src={starbucks} alt="Movie_App" className="project_img" />
-					Clone Starbucks
-				</div>
-				<div className="project">
-					<img src={movie} alt="Movie_App" className="project_img" />
-					영화 추천 사이트
-				</div>
-				<div className="project">
-					<img src={paint} alt="Paint_App" className="project_img" />
-					그림판 만들기
+const Project = () => {
+	return (
+		<article className="sub_content project" id="project">
+			<div className="content">
+				<h1 className="sub_content_title">Project</h1>
+				<div className="projects_cntr">
+					<div className="project_cntr">
+						<ProjectLayout
+							img={racstagram_v2}
+							title="Racstagram_V2"
+							desc="인스타그램 클론 프로젝트로 기존 버전 1에서 redux를 도입하였고, 더 많은 기능과 디테일이 추가되었습니다."
+							stacks={[ReactIcon, ReduxIcon, Firebase]}
+						/>
+						<ProjectLayout
+							img={racstagram_v1}
+							title="Racstagram_V1"
+							desc="인스타그램 클론 프로젝트 초창기 모델로, 당시 Context API를 사용하지 않고 구현했습니다."
+							stacks={[ReactIcon, Firebase]}
+						/>
+					</div>
+					<div className="project_cntr">
+						<ProjectLayout
+							img={paint}
+							title="Paint-Up"
+							desc="기존 'Nomadcoder 강의'의 그림판 프로젝트에서 스스로 더 많은 기능을 추가한 프로젝트입니다."
+							stacks={[HtmlIcon, CssIcon, JsIcon]}
+						/>
+						<ProjectLayout
+							img={todoList}
+							title="Romentum"
+							desc="기존 'Nomadcoder 강의'의 날씨, TodoList, 시간 앱 프로젝트에서 스스로 더 많은 기능을 추가한 프로젝트 입니다."
+							stacks={[HtmlIcon, CssIcon, JsIcon]}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
-	</article>
-);
+		</article>
+	);
+};
 
 export default Project;
