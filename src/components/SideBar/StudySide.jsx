5a33faa7ada77ef5b5d24ecf14e58e3910c8cd40
RaccoonCode96 <waawoo13@gmail.com>
2021-09-05 23:05:34 +0900

    Add: Study route 담당 side layout 컴포넌트

---
+++ src/components/SideBar/StudySide.jsx
@@ -0,0 +1,15 @@
+import { EllipsisOutlined } from '@ant-design/icons';
+
+const StudySide = ({ children }) => (
+	<>
+		<div className="active">
+			<div className="side_bar_outline tab_name_cntr">
+				<div className="tab_name">실행 및 디버그: 실행</div>
+				<EllipsisOutlined className="icon" />
+			</div>
+			{children}
+		</div>
+	</>
+);
+
+export default StudySide;
