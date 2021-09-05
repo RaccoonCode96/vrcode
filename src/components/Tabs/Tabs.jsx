24772705426f755fa806d248e5356ec23249accb
RaccoonCode96 <waawoo13@gmail.com>
2021-09-05 23:06:43 +0900

    Update: 다른 컴포넌트에서의 재사용을 위해서 선택적 a태그 사용 조건을 넣음

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,7 +2,7 @@ import './Tabs.scss';
 import { CloseOutlined } from '@ant-design/icons';
 
 export const Tab = ({ Icon, title, active, color, type, href }) => {
-	return (
+	return href ? (
 		<a
 			href={href}
 			className="tab_cntr"
@@ -22,6 +22,22 @@ export const Tab = ({ Icon, title, active, color, type, href }) => {
 				<CloseOutlined className="close" />
 			</div>
 		</a>
+	) : (
+		<div className="tab_cntr" style={active && { backgroundColor: '#1e1e1e' }}>
+			<div className="tab">
+				<Icon className="icon" style={color && { color }} />
+				<span className="tab_title" style={active && { color: '#68C977' }}>
+					{title}
+				</span>
+				<span className="title_type" style={active && { color: '#68C977' }}>
+					{type}
+				</span>
+			</div>
+			<div className="end">
+				{active && <div className="dot"></div>}
+				<CloseOutlined className="close" />
+			</div>
+		</div>
 	);
 };
 
