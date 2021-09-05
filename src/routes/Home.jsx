180fb11b82fbbeaab448506bb9fb45736d2ab48e
RaccoonCode96 <waawoo13@gmail.com>
2021-09-05 23:57:20 +0900

    Update:  Link 의 경우 hash로 스크롤 이동이 안되어 a태그로 다시 변경함

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,39 +10,57 @@ import About from '../components/Mains/About';
 import Package from '../components/Mains/Package';
 import Start from '../components/Mains/Start';
 import useMoveTop from '../hooks/useMoveTop';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
 	useMoveTop(true);
+	const { hash } = useLocation();
 	return (
 		<div className="middle">
 			<aside>
 				<Navigation>
 					<SideBar>
 						<IntroBar>
-							<Link
-								to={{
-									hash: '#about',
-								}}
-							>
-								<ContentRow Icon={RoutesIcon} title="About.jsx" />
-							</Link>
-							<Link
-								to={{
-									hash: '#stack',
-								}}
-							>
-								<ContentRow Icon={NodeIcon} title="package.json" />
-							</Link>
+							<a href="#about">
+								<ContentRow
+									Icon={RoutesIcon}
+									title="About.jsx"
+									active={hash === '#about'}
+								/>
+							</a>
+							<a href="#stack">
+								<ContentRow
+									Icon={NodeIcon}
+									title="package.json"
+									active={hash === '#stack'}
+								/>
+							</a>
 						</IntroBar>
 					</SideBar>
 				</Navigation>
 			</aside>
 			<div className="main_container">
 				<Tabs>
-					<Tab Icon={VscodeIcon} title="시작" href={'#start'} active />
-					<Tab Icon={RoutesIcon} title="About" href={'#about'} type=".jsx" />
-					<Tab Icon={NodeIcon} title="package" href={'#stack'} type=".json" />
+					<Tab
+						Icon={VscodeIcon}
+						title="시작"
+						href={'#start'}
+						active={hash === '#start' || hash === ''}
+					/>
+					<Tab
+						Icon={RoutesIcon}
+						title="About"
+						href={'#about'}
+						active={hash === '#about'}
+						type=".jsx"
+					/>
+					<Tab
+						Icon={NodeIcon}
+						title="package"
+						href={'#stack'}
+						active={hash === '#stack'}
+						type=".json"
+					/>
 				</Tabs>
 				<main>
 					<Start />
