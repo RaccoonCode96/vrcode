b4929a672f5f8e40a6cfefc003d2e3297c83fec7
RaccoonCode96 <waawoo13@gmail.com>
2021-09-05 23:11:04 +0900

    Update: project 이원화, useMoveTop 훅 적용,  a태그 에서 react router dom hash 방식으로 변경

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,46 +3,55 @@ import IntroBar from '../components/IntroBar/IntroBar';
 import Navigation from '../components/Navigation/Navigation';
 import SideBar from '../components/SideBar/SideBar';
 import Tabs, { Tab } from '../components/Tabs/Tabs';
-import { ReactComponent as ReactIcon } from '../images/react_icon.svg';
 import { ReactComponent as RoutesIcon } from '../images/routes_icon.svg';
 import { ReactComponent as NodeIcon } from '../images/node_icon.svg';
 import { ReactComponent as VscodeIcon } from '../images/vscode_icon.svg';
 import About from '../components/Mains/About';
 import Package from '../components/Mains/Package';
-import Project from '../components/Mains/Project';
 import Start from '../components/Mains/Start';
+import useMoveTop from '../hooks/useMoveTop';
+import { Link } from 'react-router-dom';
 
-const Home = () => (
-	<div className="middle">
-		<aside>
-			<Navigation>
-				<SideBar>
-					<IntroBar>
-						<a href="#about">
-							<ContentRow Icon={RoutesIcon} title="About.jsx" />
-						</a>
-						<a href="#stack">
-							<ContentRow Icon={NodeIcon} title="package.json" />
-						</a>
-					</IntroBar>
-				</SideBar>
-			</Navigation>
-		</aside>
-		<div className="main_container">
-			<Tabs>
-				<Tab Icon={VscodeIcon} title="시작" href={'#start'} active />
-				<Tab Icon={RoutesIcon} title="About" href={'#about'} type=".jsx" />
-				<Tab Icon={NodeIcon} title="package" href={'#stack'} type=".json" />
-				<Tab Icon={ReactIcon} title="Project" href={'#project'} type=".jsx" />
-			</Tabs>
-			<main>
-				<Start />
-				<About />
-				<Package />
-				<Project />
-			</main>
+const Home = () => {
+	useMoveTop(true);
+	return (
+		<div className="middle">
+			<aside>
+				<Navigation>
+					<SideBar>
+						<IntroBar>
+							<Link
+								to={{
+									hash: '#about',
+								}}
+							>
+								<ContentRow Icon={RoutesIcon} title="About.jsx" />
+							</Link>
+							<Link
+								to={{
+									hash: '#stack',
+								}}
+							>
+								<ContentRow Icon={NodeIcon} title="package.json" />
+							</Link>
+						</IntroBar>
+					</SideBar>
+				</Navigation>
+			</aside>
+			<div className="main_container">
+				<Tabs>
+					<Tab Icon={VscodeIcon} title="시작" href={'#start'} active />
+					<Tab Icon={RoutesIcon} title="About" href={'#about'} type=".jsx" />
+					<Tab Icon={NodeIcon} title="package" href={'#stack'} type=".json" />
+				</Tabs>
+				<main>
+					<Start />
+					<About />
+					<Package />
+				</main>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Home;
