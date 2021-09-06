3d82421a7bcda4283d53daa03b8596a91242288f
RaccoonCode96 <waawoo13@gmail.com>
2021-09-06 23:36:45 +0900

    Update: study side 내부 내용 추가 완료

--- src/routes/Study.jsx
+++ src/routes/Study.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import StudyMain from '../components/Mains/StudyMain';
 import Navigation from '../components/Navigation/Navigation';
 import SideBar from '../components/SideBar/SideBar';
@@ -5,19 +6,47 @@ import Tabs, { Tab } from '../components/Tabs/Tabs';
 import { ReactComponent as ReactIcon } from '../images/react_icon.svg';
 
 const Study = () => {
+	const [isAllVisible, setIsAllVisble] = useState(false);
 	return (
 		<div className="middle">
 			<aside>
 				<Navigation>
-					<SideBar></SideBar>
+					<SideBar>
+						<div className="study_side_item">
+							<button
+								className="study_side_btn"
+								onClick={() => {
+									setIsAllVisble(true);
+								}}
+							>
+								모든 설명 길게 하기
+							</button>
+							<div className="study_side_btn_desc">
+								각 종류별 기록의 설명을 모두 길게 합니다.
+							</div>
+						</div>
+						<div className="study_side_item">
+							<button
+								className="study_side_btn"
+								onClick={() => {
+									setIsAllVisble(false);
+								}}
+							>
+								모든 설명 짧게 하기
+							</button>
+							<div className="study_side_btn_desc">
+								각 종류별 기록의 설명을 모두 짧게 합니다.
+							</div>
+						</div>
+					</SideBar>
 				</Navigation>
 			</aside>
 			<div className="main_container">
 				<Tabs>
-					<Tab Icon={ReactIcon} title="Study" active />
+					<Tab Icon={ReactIcon} title="Study" type=".jsx" active />
 				</Tabs>
 				<main>
-					<StudyMain />
+					<StudyMain isAllVisible={isAllVisible} />
 				</main>
 			</div>
 		</div>
