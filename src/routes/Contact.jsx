dbd361d2161a9e12c123d61b030db04c21262589
RaccoonCode96 <waawoo13@gmail.com>
2021-09-06 23:32:24 +0900

    Add: Contact Route page 완료

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,3 +1,28 @@
-const Contact = () => <div>Contact</div>;
+import Navigation from '../components/Navigation/Navigation';
+import SideBar from '../components/SideBar/SideBar';
+import Tabs, { Tab } from '../components/Tabs/Tabs';
+import { FileOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import ContactMain from '../components/Mains/ContactMain';
+
+const Contact = () => (
+	<div className="middle">
+		<aside>
+			<Navigation>
+				<SideBar></SideBar>
+			</Navigation>
+		</aside>
+		<div className="main_container">
+			<Tabs>
+				<Link to="/contact" style={{ height: '100%' }}>
+					<Tab Icon={FileOutlined} title="Contact" active />
+				</Link>
+			</Tabs>
+			<main>
+				<ContactMain />
+			</main>
+		</div>
+	</div>
+);
 
 export default Contact;
