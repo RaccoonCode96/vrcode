0d693a4c7ffea4f92201040effadeebc01ef632b
RaccoonCode96 <waawoo13@gmail.com>
2021-09-06 23:24:25 +0900

    Update: 설명 더보기 기능 추가

--- src/components/Common/StudyRow.jsx
+++ src/components/Common/StudyRow.jsx
@@ -1,22 +1,40 @@
 import './StudyRow.scss';
 import { ReactComponent as IinkIcon } from '../../images/link_icon.svg';
+import { useEffect, useMemo, useState } from 'react';
 
-const StudyRow = ({ title, Icon, href, desc }) => (
-	<div>
-		<a
-			href={href}
-			target="_blank"
-			rel="noopener noreferrer"
-			className="study_item"
-		>
-			<div className="study_name">
+const StudyRow = ({ isAllVisible, title, Icon, href, desc }) => {
+	const [isMore, setIsMore] = useState(false);
+	const smallDesc = useMemo(() => desc && desc.slice(0, 19), [desc]);
+	useEffect(() => {
+		setIsMore(isAllVisible);
+	}, [isAllVisible]);
+	return (
+		<div className="study_item">
+			<a
+				href={href}
+				target="_blank"
+				className="study_name"
+				rel="noopener noreferrer"
+			>
 				{Icon && <Icon className="study_icon" />}
 				<h3 className="study_item_title">{title}</h3>
 				<IinkIcon className="link_icon" />
-			</div>
-		</a>
-		<p>{desc}</p>
-	</div>
-);
+			</a>
+			<p>
+				{isMore ? desc : smallDesc}
+				{!isMore && (
+					<span
+						className="study_desc_more"
+						onClick={() => {
+							setIsMore(!isMore);
+						}}
+					>
+						...더 보기
+					</span>
+				)}
+			</p>
+		</div>
+	);
+};
 
 export default StudyRow;
