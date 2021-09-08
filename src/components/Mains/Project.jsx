225db6524a0b53dd8bfca6eb818edc8ff8018a94
RaccoonCode96 <waawoo13@gmail.com>
2021-09-08 17:44:55 +0900

    Update: 프로젝트 설명 수정, 주석 추가

--- src/components/Mains/Project.jsx
+++ src/components/Mains/Project.jsx
@@ -13,6 +13,7 @@ import { ReactComponent as Firebase } from '../../images/firebase_icon.svg';
 import { Link } from 'react-router-dom';
 import { BugOutlined } from '@ant-design/icons';
 
+// Projects (Route) -> Project (main)
 const Project = () => {
 	return (
 		<article className="sub_content project" id="project">
@@ -23,13 +24,13 @@ const Project = () => {
 						<ProjectLayout
 							img={racstagram_v2}
 							title="Racstagram_V2"
-							desc="인스타그램 클론 프로젝트로 기존 버전 1에서 redux를 도입하였고, 더 많은 기능과 디테일이 추가되었습니다."
+							desc="인스타그램 클론 프로젝트로 기존 버전 1에서 Redux-toolkit을 도입하여 더 많은 기능과 디테일이 추가된 버전으로 Redux에 친숙해지고, firebase의 DB를 통해서 data를 어떻게 구축하고 관리할지 고민하고 사용해 보았으며, 다양한 기능들을 구현함으로서 전체적인 동작 방식에 대해서 익숙해 졌습니다. 하지만, 테스트 코드와 Type 설정 등에 관한 아쉬움이 많이 남는 프로젝트 입니다."
 							stacks={[ReactIcon, ReduxIcon, Firebase]}
 						/>
 						<ProjectLayout
 							img={racstagram_v1}
 							title="Racstagram_V1"
-							desc="인스타그램 클론 프로젝트 초창기 모델로, 당시 Context API를 사용하지 않고 구현했습니다."
+							desc="인스타그램 클론 프로젝트 초창기 모델로, context API 없이 firebase의 간단한 로그인, 가입을 구현하고, 게시글의 CRUD에 익숙해졌습니다. 하지만, 복잡한 상태 관리와 prop 전달로 인해 더 많은 지식이 필요함을 느꼈습니다."
 							stacks={[ReactIcon, Firebase]}
 						/>
 					</div>
@@ -37,13 +38,13 @@ const Project = () => {
 						<ProjectLayout
 							img={vrcode}
 							title="VRCode"
-							desc="개발자가 자주보는 화면인 vscode 에디터 디자인의 포트폴리오를 만들어 보았습니다."
+							desc="개발자가 자주보는 화면인 vscode 에디터 디자인의 포트폴리오를 만들어 보았습니다. 프로젝트를 통해서 머리속에 있는 자신만의 기획, 디자인, 기능 등을 실체화 시키는 좋은 경험이였습니다."
 							stacks={[ReactIcon]}
 						/>
 						<ProjectLayout
 							img={paint}
 							title="Paint-Up"
-							desc="기존 'Nomadcoder 강의'의 그림판 프로젝트에서 스스로 더 많은 기능을 추가한 프로젝트입니다."
+							desc="기존 'Nomadcoder 강의'의 그림판 프로젝트에서 스스로 더 많은 기능을 추가한 프로젝트입니다. JS를 처음 배웠을 때, 실제로 프로젝트를 만들고 기능을 추가하면서 개발에 대한 흥미를 높이며 더 많은 기술 탐구와 JS에 대한 이해에 대한 필요성을 느끼게 해주는 프로젝트 였습니다."
 							stacks={[HtmlIcon, CssIcon, JsIcon]}
 						/>
 					</div>
